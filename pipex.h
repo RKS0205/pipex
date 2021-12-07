7610a231c2a7e26acb62eb59ba66eceaf399172c
rkenji-s <rkenji-s@student.42sp.org.br>
2021-12-07 02:08:39 +0100

    testing

--- pipex.h
+++ pipex.h
@@ -1,9 +1,13 @@
 #ifndef PIPEX_H
 # define PIPEX_H
+# define STDIN 0
+# define STDOUT 1
+# define STDERR 2
 
 # include <unistd.h>
 # include <stdlib.h>
 # include <sys/wait.h>
 # include <fcntl.h>
+
 
 #endif
--- src/pipex.c
+++ src/pipex.c
@@ -1,6 +1,43 @@
-#include <stdio.h>
+#include "../pipex.h"
 
-int main(void)
+int main(int argc, char **argv, char **env)
 {
-	printf ("a");
+	int pipefd[2];
+	int	pid;
+	int	fdin;
+	int fdout;
+	char* b[] = {"grep", "hello", NULL};
+	char* a[] = {"wc", "-l", NULL};
+
+	if (argc != 5)
+		write (2, "Invalid number of arguments", 27);
+	else
+	{
+		if (access(argv[1], F_OK) == -1)
+			fdin = open(argv[1], O_CREAT | O_WRONLY);
+		else
+			fdin = open(argv[1], O_RDONLY);
+		if (access(argv[4], F_OK) == -1)
+			fdout = open(argv[4], O_CREAT | O_WRONLY);
+		else
+			fdout = open(argv[4], O_WRONLY);
+		if (pipe(pipefd) == -1)
+			return (-1);
+		pid = fork();
+		if (pid == 0)
+		{
+			close(pipefd[0]);
+			dup2(pipefd[1], STDOUT);
+			dup2(fdin, STDIN);
+			execve("/usr/bin/grep", b, env);
+		}
+		else
+		{
+			close(pipefd[1]);
+			dup2(fdout, STDOUT);
+			dup2(pipefd[0], STDIN);
+			execve("/usr/bin/wc", a, env);
+		}
+	}
+	return (0);
 }
